be06030970aa18f36c9412d05fc8cfc249826b7f
Seline <32873@ma-web.nl>
2021-09-24 10:01:46 +0200

    aardappel

---
+++ gedicht.py
@@ -0,0 +1,11 @@
+vers1 = "we stoppen je in de grond."
+vers2 = "we leggen je in de zon, regen en wind, je bent nog maar een aardappel kind."
+vers3 = "Als je goed genoeg bent ga je naar binnen."
+vers4 = "dan gaan we je wel of niet schillen, net wat we maar willen."
+vers5 = "Dikwijls gaan we hakken en bakken voor de frituur of stampen, heerlijk die geurige dampen."
+vers6 = "Soms ben je goedkoop, dan weer duur."
+vers7 = "vaak ben je te koop los of in een zak, voor een ieders gemak."
+vers8 = "Later krijg je van die sprieten, dan wordt je oud, ook nog rimpels, dan is het fout"
+vers9 = "Dan ben je niet meer om van te genieten."
+
+print (vers1,vers2,vers3,vers4,vers5,vers6,vers7,vers8,vers9)
