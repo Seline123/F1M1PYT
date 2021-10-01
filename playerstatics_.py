deb5df3b997afa401e06e7b484602bd155a5cebd
Seline <32873@ma-web.nl>
2021-10-01 10:54:30 +0200

    spel

---
+++ playerstatics_.py
@@ -0,0 +1,22 @@
+print("Player statistics") 
+Name = input ("enter name : ") 
+speed = "210"
+special_power = "Time traveling"
+car = "Grey Cupra 80"
+lives_just_left = "2"
+ammo = "345"
+battery_of_power = "30"
+level = "127"
+wanted = "true"
+XP = "345.3K"
+
+print("Statistics of " + Name)
+print("speed : " + speed)
+print("Special power : " + special_power)
+print("Car : " + car)
+print("lives left : " + lives_just_left)
+print("ammo" + ammo)
+print("Battery of power " + battery_of_power)
+print("Level : " + level)
+print("Wanted : " + wanted)
+print("XP : " + XP)
