03de8477539c6f97c9fb2e2b11206011c6f26fe5
Seline <32873@ma-web.nl>
2022-01-12 14:08:55 +0100

    python

---
+++ PYTB1L6Groceries/PYTB1L6Groceries.py
@@ -0,0 +1,10 @@
+groceries = ["milk", "cheese", "Sausage"]
+Shoppingcart = ["cheese", "pizza", "milk"]
+a = set(groceries)
+b = set(Shoppingcart)
+
+if input == groceries:
+    print("well done")
+
+else:
+    print("try again")
---
+++ floats.py
@@ -0,0 +1,13 @@
+List = [0.1, 1.2, 2.3, 3.4, 4.5, 5.6, 6.7]
+print(List[0])
+print(List[1])
+print(List[2])
+print(List[3])
+print(List[4])
+print(List[5])
+print(List[6])
+
+List.append(1.2)
+print(List)
+
+List.pop(     )
